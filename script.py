bf15b2491ff83b7568caa9c0d7b2c42db7ec849c
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-04-07 15:27:39 +0530

    script.py

--- script.py
+++ script.py
@@ -1,24 +1,24 @@
 class script(object):
 
-    START_MSG = """ <b>Hi {}
+    START_MSG = """ <b>Hello! {}
 
-I'm a Image Editor Bot which Supports various modes
+This is a Image Editor Bot which Supports various modes.... 
 
-For more click help....</b>"""
-    HELP_MSG = """Hai, Follow these Steps..
+For more click /help....</b>"""
+    HELP_MSG = """Hey!, Follow these Steps..
 
-<code>🌀 Send me any Image to Edit..</code>
+<code>☢ Send me any Image to Edit..</code>
 
-<code>🌀 Select the Corresponding mode that you need</code>
+<code>☢ Select the Corresponding mode that you need</code>
 
-<code>🌀 Your Edited Image will be Uploaded </code>
+<code>☢ Your Edited Image will be Uploaded </code>
 
 
-© @TroJanzHEX"""
+© @TeleRoidGroup"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://telegram.dog/Moviesflixers_DL'>Click Here</a>"""
